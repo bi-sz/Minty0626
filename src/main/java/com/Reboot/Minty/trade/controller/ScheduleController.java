5157366c4a91bdee03db7a5d1114ae000afcfbbd
bi <kwonbe99@gmail.com>
2023-06-19 15:54:42 +0900

    스케줄 수정 완료

--- src/main/java/com/Reboot/Minty/trade/controller/ScheduleController.java
+++ src/main/java/com/Reboot/Minty/trade/controller/ScheduleController.java
@@ -2,7 +2,10 @@ package com.Reboot.Minty.trade.controller;
 
 import com.Reboot.Minty.member.entity.User;
 import com.Reboot.Minty.member.service.UserService;
+import com.Reboot.Minty.trade.entity.Schedule;
 import com.Reboot.Minty.trade.entity.Trade;
+import com.Reboot.Minty.trade.repository.ScheduleRepository;
+import com.Reboot.Minty.trade.service.ScheduleService;
 import com.Reboot.Minty.trade.service.TradeService;
 import com.Reboot.Minty.tradeBoard.service.TradeBoardService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,14 +28,18 @@ public class ScheduleController {
     private final TradeService tradeService;
 
     private final TradeBoardService tradeBoardService;
+    private final UserService userService;
+    private final ScheduleRepository scheduleRepository;
+
+    private final ScheduleService scheduleService;
 
     @Autowired
-    private UserService userService;
-
-    @Autowired
-    public ScheduleController(TradeService tradeService, TradeBoardService tradeBoardService) {
+    public ScheduleController(TradeService tradeService, TradeBoardService tradeBoardService, UserService userService, ScheduleRepository scheduleRepository, ScheduleService scheduleService) {
         this.tradeService = tradeService;
         this.tradeBoardService = tradeBoardService;
+        this.userService = userService;
+        this.scheduleRepository = scheduleRepository;
+        this.scheduleService = scheduleService;
     }
 
     @GetMapping("/schedule/{tradeId}")
@@ -43,12 +50,66 @@ public class ScheduleController {
         String role = tradeService.getRoleForTrade(tradeId, userId);
         User buyer= userService.getUserInfoById(trade.getBuyerId().getId());
         User seller= userService.getUserInfoById(trade.getSellerId().getId());
+        Schedule buyerSchedule = scheduleRepository.findByUserId(buyer);
+        Schedule sellerSchedule = scheduleRepository.findByUserId(seller);
+
+        boolean buyerCheckDay = false;
+        boolean buyerCheckArea = false;
+        boolean buyerCheckDuration = false;
+        boolean buyerCheckIntroduction = false;
+        boolean sellerCheckDay = false;
+        boolean sellerCheckArea = false;
+        boolean sellerCheckDuration = false;
+        boolean sellerCheckIntroduction = false;
+
+        if (buyerSchedule != null) {
+            if (buyerSchedule.getHopeDay() != null) {
+                buyerCheckDay = scheduleService.checkDay(buyerSchedule, buyerSchedule.getHopeDay());
+            }
+            if (buyerSchedule.getHopeArea() != null) {
+                buyerCheckArea = scheduleService.checkArea(buyerSchedule, buyerSchedule.getHopeArea());
+            }
+            if (buyerSchedule.getScheduleDuration() != null) {
+                buyerCheckDuration = scheduleService.checkDuration(buyerSchedule, buyerSchedule.getScheduleDuration());
+            }
+            if (buyerSchedule.getIntroduction() != null) {
+                buyerCheckIntroduction = scheduleService.checkIntroduction(buyerSchedule, buyerSchedule.getIntroduction());
+            }
+        }
+
+        if (sellerSchedule != null) {
+            if (sellerSchedule.getHopeDay() != null) {
+                sellerCheckDay = scheduleService.checkDay(sellerSchedule, sellerSchedule.getHopeDay());
+            }
+            if (sellerSchedule.getHopeArea() != null) {
+                sellerCheckArea = scheduleService.checkArea(sellerSchedule, sellerSchedule.getHopeArea());
+            }
+            if (sellerSchedule.getScheduleDuration() != null) {
+                sellerCheckDuration = scheduleService.checkDuration(sellerSchedule, sellerSchedule.getScheduleDuration());
+            }
+            if (sellerSchedule.getIntroduction() != null) {
+                sellerCheckIntroduction = scheduleService.checkIntroduction(sellerSchedule, sellerSchedule.getIntroduction());
+            }
+        }
+
+        model.addAttribute("sellerSchedule", sellerSchedule);
+        model.addAttribute("buyerSchedule", buyerSchedule);
+        model.addAttribute("sellerCheckDay", sellerCheckDay);
+        model.addAttribute("sellerCheckArea", sellerCheckArea);
+        model.addAttribute("sellerCheckDuration", sellerCheckDuration);
+        model.addAttribute("sellerCheckIntroduction", sellerCheckIntroduction);
+        model.addAttribute("buyerCheckDay", buyerCheckDay);
+        model.addAttribute("buyerCheckArea", buyerCheckArea);
+        model.addAttribute("buyerCheckDuration", buyerCheckDuration);
+        model.addAttribute("buyerCheckIntroduction", buyerCheckIntroduction);
 
         model.addAttribute("trade", trade);
         model.addAttribute("role",role);
         model.addAttribute("buyer",buyer);
         model.addAttribute("seller",seller);
         model.addAttribute("tradeId", tradeId);
+        model.addAttribute("buyerSchedule",buyerSchedule);
+        model.addAttribute("sellerSchedule",sellerSchedule);
         return "trade/schedule";
     }
 

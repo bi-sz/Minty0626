227195bd9be84d0cdc9e9798d9c72d7c9138c199
bi <kwonbe99@gmail.com>
2023-06-19 15:57:45 +0900

    헤더css, 유저샵 컨트롤러 수정

--- src/main/java/com/Reboot/Minty/member/controller/UserShopController.java
+++ src/main/java/com/Reboot/Minty/member/controller/UserShopController.java
@@ -5,6 +5,7 @@ import com.Reboot.Minty.member.repository.UserRepository;
 import com.Reboot.Minty.member.service.UserService;
 import com.Reboot.Minty.review.entity.Review;
 import com.Reboot.Minty.review.service.ReviewService;
+import jakarta.persistence.EntityExistsException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserShopController {
@@ -27,24 +29,37 @@ public class UserShopController {
     @Autowired
     private UserService userService;
 
-    @GetMapping(value = "usershop")
-    public String usershop(HttpServletRequest request, Model model) {
-        HttpSession session = request.getSession();
-        String userEmail = (String) session.getAttribute("userEmail");
-        User user = userService.getUserInfo(userEmail);
-        Long userId = user.getId();
+    @GetMapping(value = {"usershop", "usershop/{userId}"})
+    public String usershop(HttpServletRequest request, Model model, Optional<Long> otherUserId) {
+        if(!otherUserId.isPresent()) {
+            HttpSession session = request.getSession();
+            String userEmail = (String) session.getAttribute("userEmail");
+            User user = userService.getUserInfo(userEmail);
+            Long userId = user.getId();
 
-        List<Review> receivedReviews = reviewService.getReceivedReviewsByReceiverIdOrderByWriteTimeDesc(user);
+            List<Review> receivedReviews = reviewService.getReceivedReviewsByReceiverIdOrderByWriteTimeDesc(user);
 
-        if (user != null) {
-            model.addAttribute("user", user);
-            model.addAttribute("receivedReviews", receivedReviews);
-        }else {
-            model.addAttribute("errorMessage", "회원 정보를 찾을 수 없습니다.");
+            if (user != null) {
+                model.addAttribute("user", user);
+                model.addAttribute("receivedReviews", receivedReviews);
+            } else {
+                model.addAttribute("errorMessage", "회원 정보를 찾을 수 없습니다.");
+            }
+
+            return "member/userShop";
+
+        }else{
+            User user = userService.getUserById(otherUserId.orElseThrow(EntityExistsException::new));
+            List<Review> receivedReviews = reviewService.getReceivedReviewsByReceiverIdOrderByWriteTimeDesc(user);
+            if (user != null) {
+                model.addAttribute("user", user);
+                model.addAttribute("receivedReviews", receivedReviews);
+            } else {
+                model.addAttribute("errorMessage", "회원 정보를 찾을 수 없습니다.");
+            }
+
+            return "member/userShop";
         }
-
-        return "member/userShop";
     }
-
 
 }
